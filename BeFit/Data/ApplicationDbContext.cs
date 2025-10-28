7d0cb9a078ef7453b9aaf22943f52b86d423d7a4
Dejmenek <dejmen14567@gmail.com>
2025-10-28 18:12:15 +0100

    feat: add DbSet properties to ApplicationDbContext
    This update introduces several new DbSet properties in the
    `ApplicationDbContext` class for managing entities such as
    `WorkoutTemplate`, `WorkoutTemplateExercise`, `Exercise`,
    `WorkoutSessionDetails`, and `WorkoutSession`. The
    `OnModelCreating` method has also been modified to ensure
    proper configuration of the model by calling
    `base.OnModelCreating(builder)` and applying configurations
    from the assembly.

--- BeFit/Data/ApplicationDbContext.cs
+++ BeFit/Data/ApplicationDbContext.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using BeFit.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace BeFit.Data
+namespace BeFit.Data;
+
+public class ApplicationDbContext : IdentityDbContext
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public DbSet<WorkoutTemplate> WorkoutTemplates { get; set; }
+    public DbSet<WorkoutTemplateExercise> WorkoutTemplateExercises { get; set; }
+    public DbSet<Exercise> Exercises { get; set; }
+    public DbSet<WorkoutSessionDetails> WorkoutSessionDetails { get; set; }
+    public DbSet<WorkoutSession> WorkoutSessions { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 }
